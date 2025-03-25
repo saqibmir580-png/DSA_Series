ed6f3f2d6d2fa9c4f64b4fa019b606d27eb15eab
unknown <saqibmir580@gmail.com>
2025-03-25 02:21:23 -0700

    Basic

---
+++ Basic/operators.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+//     //post increment
+//     int a=10;
+//     ++a;
+//     cout<<a<<endl;
+//     //post decrement
+//     --a;
+//     cout<<a<<endl;
+
+//moduls operators=>gives the remaider
+// int a=15%2;
+
+// cout<<a;
+// int b=12%40;
+// cout<<b;
+
+
+ }
---
+++ Basic/variable.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    float b;
+    char c;
+a=12;
+b=3.3;
+c='s';
+//    // cout<<a;
+//     cout<<c;
+//     cout<<b;
+    //update the variable
+    // a=a+10;
+    // cout<<a;
+    //a=a-2
+    //a=a*3;
+    //shorthand
+   // a+=10;
+    //cout<<a;
+
+
+    return 0;
+}
